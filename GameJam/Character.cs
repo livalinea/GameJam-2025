f4e9421299e32dd2bd9e02f1154baa7d87bd2208
Liva Joensen <livalinea@gmail.dk>
2025-10-21 09:25:45 +0200

    Character

--- GameJam/Character.cs
+++ GameJam/Character.cs
@@ -8,5 +8,29 @@ namespace GameJam
 {
     public class Character
     {
+    private const int DefaultLives = 10; 
+        public string Name { get; } = "Mouse";
+        public int Lives { get; private set; }
+
+        public Character()
+        {
+            Lives = DefaultLives;
+        }
+
+        // Tjek om spilleren lever
+        public bool IsAlive() => Lives > 0;
+
+        // Spilleren tager skade
+        public void TakeDamage(int amount)
+        {
+            Lives -= amount;
+            if (Lives < 0) Lives = 0;
+            Console.WriteLine($"{Name} mister {amount} liv! ({Lives} HP tilbage)");
+
+            if (!IsAlive())
+            {
+                Console.WriteLine($"{Name} er død! Game Over!");
+            }
+        }
     }
 }
